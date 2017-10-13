b514883d55fbd89c7cdd4e1abf9900e6bb1ef38a
Jussi <jussi.hietanen@aalto.fi>
2017-10-13 08:48:01 -0700

    Update PTTdisabler.c

--- PTTdisabler.c
+++ PTTdisabler.c
@@ -5,30 +5,36 @@
  
 #define BUF_SIZE 256
  
-int is_ptt(void) {
+int is_ptt(void)
+{
         char buffer[BUF_SIZE] = {'\0'};
         NU_Current_Dir("A:", buffer);
         return strcmp(buffer, "\\documents\\");
 }
  
-int copy_file(const char *src, const char *dst) {
+int copy_file(const char *src, const char *dst)
+{
         FILE *in = fopen(src, "rb"), *out;
         int c;
         if (!in)
                 return -1;
         out = fopen(dst, "wb");
-        if (!out) {
+        if (!out) 
+       {
                 fclose(in);
                 return -1;
         }
         while ((c = fgetc(in)) != EOF)
+        {
                 fputc(c, out);
+        }
         fclose(in);
         fclose(out);
         return 0;
 }
  
-void copy_files(void) {
+void copy_files(void)
+{
         struct dstat file;
         NU_Get_First(&file, SRC_DIR "*.*");
         do {
@@ -41,8 +47,12 @@ void copy_files(void) {
         NU_Done(&file);
 }
  
-int main(void) {
+int main(void)
+{
         if (is_ptt())
+        {
                 copy_files();
+        }
+
         return EXIT_SUCCESS;
 }
